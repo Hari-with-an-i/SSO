48b4cdb7c39d8140287019b6afce4f8fcfe0a637
Hari-with-an-i <harin6988@gmail.com>
2025-08-10 13:13:29 +0530

    pwa changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,22 +9,22 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Serious Studies',
+        name: 'Serious Studies Only',
         short_name: 'SSO',
         description: 'A private scrapbook for you and your partner.',
-        theme_color: '#9CAF88',
-        background_color: '#ffffff',
+        theme_color: '#2b133d', // Matches the dark theme
+        background_color: '#0a0c27', // Matches the dark theme
         start_url: '/',
-        display: 'standalone',
+        display: 'standalone', // This tells the browser to open as a standalone app
         scope: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: './pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: './pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
